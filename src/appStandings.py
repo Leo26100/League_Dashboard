8df4e44a1b5af264b73186b79045746b1390c76b
Hector Alvarado <hlalvara@gmail.com>
2020-06-29 21:53:09 -0700

    MAde modifications to ScoreBoard Table

--- src/appStandings.py
+++ src/appStandings.py
@@ -37,19 +37,20 @@ index_page = html.Div(
                 ),
             ]
         ),
-        html.Div(id="region-table"),
-        html.Div(
-            [
-                dcc.Dropdown(
-                    id="axis",
-                    options=[{"label": i, "value": i} for i in graphHeaders],
-                    value="Gold",
-                )
-            ]
-        ),
-        dcc.Graph(id="region-graph"),
         html.Br(),
-        html.Div(id="pickbans"),
+        html.Div(id="region-table"),
+        # html.Div(
+        #     [
+        #         dcc.Dropdown(
+        #             id="axis",
+        #             options=[{"label": i, "value": i} for i in graphHeaders],
+        #             value="Gold",
+        #         )
+        #     ]
+        # ),
+        # dcc.Graph(id="region-graph"),
+        html.Br(),
+        # html.Div(id="pickbans"),
     ]
 )
 
@@ -61,38 +62,70 @@ index_page = html.Div(
 def update_table_output(region):
     standingsData_layout = standingsData[region]
     tournament_title = standingsData_layout["Tournament"][0]
-    standingsData_layout = standingsData_layout.drop(columns=["Tournament"])
+    # standingsData_layout = standingsData_layout.drop(columns=["Tournament"])
+    standingsData_layout = (
+        standingsData_layout.groupby(["Team"])
+        .mean()
+        .reset_index()
+        .sort_values(by=["Won"], ascending=False)
+        .round(2)
+    )
     data = standingsData_layout.to_dict("records")
     columns = [{"name": i, "id": i} for i in (standingsData_layout.columns)]
-    return (html.H1(tournament_title), tb.DataTable(data=data, columns=columns))
-
-
-@app.callback(
-    dash.dependencies.Output("region-graph", "figure"),
-    [
-        dash.dependencies.Input("region", "value"),
-        dash.dependencies.Input("axis", "value"),
-    ],
-)
-def update_graph_output(region, axis):
-    standingsDataGraph = standingsData[region]
-    standingsDataGraph = standingsDataGraph.groupby(["Team"]).mean()
-    fig = go.Figure(
-        data=[go.Bar(y=standingsDataGraph[axis], x=standingsDataGraph.index.values)],
-        layout_title_text='Average "{}"'.format(axis),
+    return (
+        html.H1(tournament_title),
+        tb.DataTable(
+            data=data,
+            columns=columns,
+            sort_action="native",
+            style_cell={"textAlign": "right"},
+            style_cell_conditional=[{"if": {"column_id": "Team"}, "textAlign": "left"}],
+            style_data_conditional=[
+                {
+                    "if": {"filter_query": "{Won} < 0.5", "column_id": "Won"},
+                    "backgroundColor": "tomato",
+                    "color": "back",
+                },
+                {
+                    "if": {"row_index": "odd"},
+                    "background": "rgb(248,248,248)",
+                    "fontWeight": "bold",
+                },
+            ],
+            style_header={
+                "backgroundColor": "rgb(230, 230, 230)",
+                "fontWeight": "bold",
+            },
+        ),
     )
-    return fig
 
 
-@app.callback(
-    dash.dependencies.Output("pickbans", "children"),
-    [dash.dependencies.Input("region", "value")],
-)
-def update_pick_bans(region):
-    pickBansTable = pickBansData[region]
-    data = pickBansTable.to_dict("records")
-    columns = [{"name": i, "id": i} for i in (pickBansTable.columns)]
-    return tb.DataTable(data=data, columns=columns)
+# @app.callback(
+#     dash.dependencies.Output("region-graph", "figure"),
+#     [
+#         dash.dependencies.Input("region", "value"),
+#         dash.dependencies.Input("axis", "value"),
+#     ],
+# )
+# def update_graph_output(region, axis):
+#     standingsDataGraph = standingsData[region]
+#     standingsDataGraph = standingsDataGraph.groupby(["Team"]).mean()
+#     fig = go.Figure(
+#         data=[go.Bar(y=standingsDataGraph[axis], x=standingsDataGraph.index.values)],
+#         layout_title_text='Average "{}"'.format(axis),
+#     )
+#     return fig
+
+
+# @app.callback(
+#     dash.dependencies.Output("pickbans", "children"),
+#     [dash.dependencies.Input("region", "value")],
+# )
+# def update_pick_bans(region):
+#     pickBansTable = pickBansData[region]
+#     data = pickBansTable.to_dict("records")
+#     columns = [{"name": i, "id": i} for i in (pickBansTable.columns)]
+#     return tb.DataTable(data=data, columns=columns)
 
 
 if __name__ == "__main__":
