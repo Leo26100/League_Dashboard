cf38d349c0117e09f1a10cadf5c582afdb856e2c
Hector Alvarado <hlalvara@gmail.com>
2020-07-06 01:02:55 -0700

    added bootstrap LUX theme to app

--- src/app.py
+++ src/app.py
@@ -1,10 +1,11 @@
 import dash
+import dash_bootstrap_components as dbc
 
 external_stylesheets = ["https://codepen.io/chriddyp/pen/bWLwgP.css"]
 
 app = dash.Dash(
     __name__,
-    external_stylesheets=external_stylesheets,
+    external_stylesheets=[dbc.themes.LUX],  # external_stylesheets,
     suppress_callback_exceptions=True,
 )
 server = app.server
